5cf15982494caf79aebf855a327a6d41607d4d9f
Dave <davenyambune@gmail.com>
2023-02-21 16:19:32 +0300

    first commit

---
+++ Exercise-two.py
@@ -0,0 +1,2 @@
+num1 =int(input("Enter your age"))
+print(f"Age : {num1} ... You have lived {num1*12} months")
---
+++ eXERCISE-THREE.py
@@ -0,0 +1,2 @@
+num1 = int(input("Enter years in months"))
+print(f"your age is {num1} you have lived {num1/int(12)} years")
